cdb7363a2262c3223de73a914088f5ff840c5bf0
Kyle Meyer <kyle@kyleam.com>
2014-04-01 01:15:02 -0400

    Expand commentary

--- bog.el
+++ bog.el
@@ -24,7 +24,23 @@
 ;;; Commentary:
 ;;
 ;; Bog provides a few convenience functions for taking research notes in
-;; Org mode. See README (or bog-readme.org) for more information.
+;; Org mode. Many of these commands center around a citekey, the unique
+;; identifier for a study. Some of the main commands are listed below.
+;;
+;; - `bog-find-citekey-pdf'
+;; - `bog-find-citekey-bib'
+;; - `bog-search-citekey-on-web'
+;; - `bog-rename-staged-pdf-to-citekey'
+;; - `bog-goto-citekey-heading-in-buffer'
+;; - `bog-goto-citekey-heading-in-notes'
+;; - `bog-search-notes-for-citekey'
+;; - `bog-clean-and-rename-staged-bibs'
+;; - `bog-refile'
+;; - `bog-search-notes'
+;; - `bog-sort-topic-headings-in-buffer'
+;; - `bog-sort-topic-headings-in-notes'
+;;
+;; See README (or bog-readme.org) for more information.
 
 ;;; Code:
 
