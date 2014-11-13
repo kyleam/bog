41744c17e4ec8c413907912747f73cd5d74d631f
Kyle Meyer <kyle@kyleam.com>
2014-11-12 23:06:38 -0500

    Shorten commentary

--- bog.el
+++ bog.el
@@ -25,22 +25,8 @@
 ;;
 ;; Bog provides a few convenience functions for taking research notes in
 ;; Org mode. Many of these commands center around a citekey, the unique
-;; identifier for a study. Some of the main commands are listed below.
-;;
-;; - `bog-find-citekey-file'
-;; - `bog-find-citekey-bib'
-;; - `bog-search-citekey-on-web'
-;; - `bog-rename-staged-file-to-citekey'
-;; - `bog-goto-citekey-heading-in-buffer'
-;; - `bog-goto-citekey-heading-in-notes'
-;; - `bog-search-notes-for-citekey'
-;; - `bog-clean-and-rename-staged-bibs'
-;; - `bog-refile'
-;; - `bog-search-notes'
-;; - `bog-sort-topic-headings-in-buffer'
-;; - `bog-sort-topic-headings-in-notes'
-;;
-;; See README (https://github.com/kyleam/bog) for more information.
+;; identifier for a study. See the README
+;; (https://github.com/kyleam/bog) for more information.
 
 ;;; Code:
 
